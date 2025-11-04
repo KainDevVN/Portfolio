da8f5609cf77195c9cd55f3ac4112bdf5186cff6
Khánh Nhật <kainvn4bussiness@gmail.com>
2025-11-04 15:36:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const words = ["Web Developer", "New Software Developer", "HCMUTE Student"];
+let i = 0, j = 0, currentWord = "", isDeleting = false;
+const el = document.querySelector(".typing");
+
+function type() {
+  currentWord = words[i];
+  el.textContent = currentWord.slice(0, j);
+  j += isDeleting ? -1 : 1;
+
+  if (!isDeleting && j === currentWord.length) return setTimeout(() => isDeleting = true, 1000), setTimeout(type, 200);
+  if (isDeleting && j === 0) return isDeleting = false, i = (i + 1) % words.length, setTimeout(type, 200);
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+type();
+
+// Smooth scrolling for navigation links
